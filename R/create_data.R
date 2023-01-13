dfb820962dad966c579f8e922769ae1b32b75889
Rob Marty <ramarty@email.wm.edu>
2023-01-13 11:42:24 -0500

    new data

--- R/create_data.R
+++ R/create_data.R
@@ -9,11 +9,15 @@ if(F){
   VNP46A3_df <- VNP46A3_df %>%
     dplyr::mutate(month = day %>% month_start_day_to_month())
   
+  closeAllConnections()
+  
   write.csv(VNP46A3_df, "~/Documents/Github/download_blackmarble/data/VNP46A3.csv", row.names = F)
   
   # Annual: VNP46A4 ---------------------------------------------------------------------
   VNP46A4_df <- create_dataset_name_df(product_id = "VNP46A4",
                                        all = T)
+  
+  closeAllConnections()
   
   write.csv(VNP46A4_df, "~/Documents/Github/download_blackmarble/data/VNP46A4.csv", row.names = F)
   
